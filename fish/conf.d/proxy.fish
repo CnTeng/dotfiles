45efbe5e395a1b892690b39598466718b90d5fea
CnTeng <tengyufei@live.com>
2022-09-20 00:57:52 +0800

    add the config of fish

---
+++ fish/conf.d/proxy.fish
@@ -0,0 +1,51 @@
+# Set the host_ip and proxy port
+set host_ip "127.0.0.1"
+set socks_port 10808
+set http_port 10809
+
+# Set the name of wsl and pad
+set wsl_name "rxdell"
+set pad_name "rxsf"
+
+# Set proxy for shell tools such as apt
+function global_proxy
+  set -gx http_proxy http://$host_ip:$http_port
+  set -gx https_proxy http://$host_ip:$http_port
+end
+
+# Set proxy for git
+function git_proxy
+  git config --global --unset user.name
+  git config --global --unset user.email
+  git config --global --unset http.proxy
+  git config --global --unset https.proxy
+  git config --global user.name "CnTeng"
+  git config --global user.email "tengyufei@live.com"
+  git config --global http.proxy "socks5://$host_ip:$socks_port"
+  git config --global https.proxy "socks5://$host_ip:$socks_port"
+end
+
+# Set proxy for npm
+function npm_proxy
+  npm config delete proxy
+  npm config delete https-proxy
+  npm config set proxy "socks5://$host_ip:$socks_port"
+  npm config set https-proxy "socks5://$host_ip:$socks_port"
+end
+
+# Set proxy which is login
+if status --is-login
+  switch $hostname 
+    case $wsl_name
+      set host_ip (cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
+      set socks_port 10810
+      set http_port 10811
+      global_proxy
+      git_proxy
+      npm_proxy
+    case $pad_name
+      global_proxy
+      git_proxy
+      npm_proxy
+  end
+end
